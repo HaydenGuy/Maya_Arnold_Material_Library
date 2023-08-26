a09ea136740c096d0300a4351228d6fac7a713d0
desktop <haydenguyn@gmail.com>
2023-08-26 15:54:41 -0700

    Added method to import .ma/.mb materials from a folder path

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append('/mnt/32346261-2a77-4ea4-ad97-df46c23e0f72/Maya_Scripts/Material_Library')
 
+import os
 import importlib
 import maya.cmds as cmds
 from PySide2.QtWidgets import QMainWindow, QApplication, QListWidgetItem
@@ -19,14 +20,30 @@ class MaterialLibrary(QMainWindow, Ui_material_window):
         super().__init__()
         self.setupUi(self)
 
+        materials_folder = "/mnt/32346261-2a77-4ea4-ad97-df46c23e0f72/Maya/Material_library/Materials"
+        self.import_materials_from_folder(materials_folder)
+
         self.list_arnold_materials()
+
+    def import_materials_from_folder(self, folder_path):
+        '''
+        Walk through the directory tree rooted at 'folder_path'
+    # 'root' is the current directory being visited
+    # 'dirs' is a list of subdirectories in the current directory
+    # 'files' is a list of filenames in the current directory
+        '''
+        for root, dirs, files in os.walk(folder_path):
+            for file in files:
+                if file.endswith(".ma") or file.endswith(".mb"):
+                    file_path = os.path.join(root, file)
+                    cmds.file(file_path, i=True)
 
     # Gets a list of all arnold materials
     def list_arnold_materials(self):
         # Get a list of all shading engines in the scene
         shading_engines = cmds.ls(type='shadingEngine')
         arnold_shaders = []
-    
+
         for sg in shading_engines:
             # Find the shader connected to the shading engine's surfaceShader attribute
             shader_connections = cmds.listConnections(sg + '.surfaceShader', source=True, destination=False)
